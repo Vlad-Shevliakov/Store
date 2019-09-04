068cf83d057e7eb6b0a60c858161953df85c83f8
Shevlyakov Vladislav <vladislavs319@gmail.com>
2019-09-04 23:56:20 +0300

    new item cart styles

--- src/pages/Cart/Orders/OrderItem/OrderItem.js
+++ src/pages/Cart/Orders/OrderItem/OrderItem.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './OrderItem.scss'
 import Button from '../../../../components/UI/Button/Button'
 
 const OrderItem = props => {
+
+
+    const [amout, setAmout] = useState(1)
 
     const {
         title,
@@ -13,18 +16,68 @@ const OrderItem = props => {
     } = props
 
 
-   const removeFunc = () => {
+    const removeFunc = () => {
         removeHandler(code)
-   }
+    }
+
+    const changeAmount = event => {
+       setAmout(+event.target.value)
+       console.log(typeof +event.target.value)
+    }
+
+    const plusOne = () => {
+       setAmout(amout + 1)
+    }
+
+    const subOne = () => {
+        if (amout <= 1) {
+            return
+        }
+        setAmout(amout - 1)
+    }
 
     return (
         <li className={classes.order_item}>
-            <div className={classes.product_img}>
-                <img src={image} alt={title}/>
-            </div>
 
-            <p>{title}</p>
-            <ul className={classes.price_c_list} >
+            <div className={classes.first_group}>
+                <div className={classes.img_wrp}>
+                    <div className={classes.product_img}>
+                        <img src={image} alt={title}/>
+                    </div>
+                </div>
+                <ul className={classes.info_list}>
+                    <li className={classes.title}>
+                        <a href="##">
+                            {title}
+                        </a>
+                    </li>
+                    <li className={classes.order_code}>
+                        код: {code}
+                    </li>
+                    <li className={classes.product_price}>
+                        Цена:&nbsp;
+                        <strong className={classes.price}>{price}</strong>
+                        <span className={classes.currency}>грн</span>
+                    </li>
+                </ul>
+                <ul className={classes.price_c_list} >
+                    <li className={classes.order_controls}>
+                        <Button classType='numControl_l' func={subOne} />
+                        <input 
+                            type="number"
+                            value={amout}
+                            className={classes.order_inp}
+                            onChange={changeAmount}
+                        />
+                        <Button classType='numControl_r' func={plusOne} />
+                    </li>
+                    <li className={classes.product_price}>
+                        <strong className={classes.price}>{price}</strong>
+                        <span className={classes.currency}>грн</span>
+                    </li>
+                </ul>
+            </div>
+            {/* <ul className={classes.price_c_list} >
                 <li className={classes.order_controls}>
                     <Button classType='numControl_l' />
                     <input 
@@ -38,7 +91,7 @@ const OrderItem = props => {
                     <strong className={classes.price}>{price}</strong>
                     <span className={classes.currency}>грн</span>
                 </li>
-            </ul>
+            </ul> */}
             <Button
                 classType='CartRemoveButton'
                 func={removeFunc}
